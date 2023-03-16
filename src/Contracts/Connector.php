c13e9947c70ec233639e2ee590d73486dc4faf53
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-16 21:56:56 +0100

    Updated Connector contracts with a constant identifying the name of the connector/provider

--- src/Contracts/Connector.php
+++ src/Contracts/Connector.php
@@ -6,5 +6,18 @@ use Illuminate\Support\Collection;
 
 interface Connector
 {
+    /**
+     * The name of the connector/provider.
+     * It will be used to identify the connector in the database and to provide
+     * an easy way to identify the connector.
+     */
+    public const NAME = 'base';
+
+    /**
+     * List all models available for this connector.
+     * Typically this function will retrieve the list though the API of the provider.
+     * Should return a collection of ModelObject objects.
+     * @return Collection
+     */
     public function listModels(): Collection;
 }
