9f9180d0af72f7ec77a3a8e742f91073cb071e0c
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-18 00:19:11 +0100

    Adds first version of complete and chat method on connector

--- src/Connectors/OpenAIConnector.php
+++ src/Connectors/OpenAIConnector.php
@@ -2,6 +2,7 @@
 
 namespace Illegal\LaravelAI\Connectors;
 
+use Exception;
 use Illegal\LaravelAI\Contracts\Connector;
 use Illegal\LaravelAI\Enums\Provider;
 use Illegal\LaravelAI\Bridges\ModelBridge;
@@ -35,5 +36,55 @@ class OpenAIConnector implements Connector
                 ->withName($model->id ?? '')
                 ->withExternalId($model->id ?? '');
         });
+    }
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    public function complete(string $model, string $prompt, int $maxTokens, float $temperature): void
+    {
+        $response = $this->client->completions()->create([
+            'model'       => $model,
+            'prompt'      => $prompt,
+            'max_tokens'  => $maxTokens,
+            'temperature' => $temperature,
+        ]);
+
+        foreach ($response->choices as $result) {
+            var_dump($result->text);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    public function chat(string $model, array|string $messages): array
+    {
+        $messages = is_array($messages) ? $messages : [
+            [
+                'role'    => 'user',
+                'content' => $messages
+            ]
+        ];
+
+        $chat = $this->client->chat()->create([
+            'model'    => $model,
+            'messages' => $messages
+        ]);
+
+        $response = [
+            'external_id' => $chat->id,
+        ];
+
+        foreach ($chat->choices as $choice) {
+            $response['message'] = [
+                'role'    => $choice->message->role,
+                'content' => $choice->message->content
+            ];
+        }
+
+        return $response;
     }
 }
--- src/Contracts/Connector.php
+++ src/Contracts/Connector.php
@@ -17,7 +17,18 @@ interface Connector
      * List all models available for this connector.
      * Typically this function will retrieve the list though the API of the provider.
      * Should return a collection of ModelObject objects.
+     *
      * @return Collection
      */
     public function listModels(): Collection;
+
+    /**
+     * Complete a prompt with the given model.
+     */
+    public function complete(string $model, string $prompt, int $maxTokens, float $temperature): void;
+
+    /**
+     * @return array<string, array<string, string>> - The response from the provider
+     */
+    public function chat(string $model, array|string $messages): array;
 }
