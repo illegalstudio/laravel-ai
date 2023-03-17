8a081f5dc8e265e830646c7b66c788df895931b1
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-17 22:39:38 +0100

    Adds new required static new function

--- src/Contracts/Bridge.php
+++ src/Contracts/Bridge.php
@@ -12,6 +12,11 @@ use Illuminate\Database\Eloquent\Model;
 interface Bridge
 {
     /**
+     * Return a new instance of the bridge.
+     */
+    public static function new(): self;
+
+    /**
      * Set the provider for the bridge.
      * This is implemented in the HasProvider trait.
      * Bridges should implement this interface and use the trait.
