f29b41abbd19b7d649199fcbcd322bb4d0186a19
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-17 19:00:00 +0100

    Improved docblock

--- src/Contracts/Bridge.php
+++ src/Contracts/Bridge.php
@@ -6,6 +6,7 @@ use Illegal\LaravelAI\Enums\Provider;
 
 /**
  * The bridge is a way to connect the AI provider to the application.
+ * It takes care of translating the AI provider's response to a format that the application can understand.
  */
 interface Bridge
 {
