403bd0f6c4ee3117918544a02325cb355cbf9856
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-18 14:13:35 +0100

    Adds Common reponse format objects, to be used by all provider

---
+++ src/Responses/MessageResponse.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Illegal\LaravelAI\Responses;
+
+use Illegal\LaravelAI\Contracts\HasNew;
+
+/**
+ * This is the common message response to which all responses, from providers, are converted
+ */
+class MessageResponse
+{
+    use HasNew;
+
+    /**
+     * @var string $role - The role of the user sending the message
+     */
+    private string $role;
+
+    /**
+     * @var string $content - The content of the message
+     */
+    private string $content;
+
+    /**
+     * Setter for the role
+     */
+    public function withRole(string $role): self
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+    /**
+     * Getter for the role
+     */
+    public function role(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * Setter for the content
+     */
+    public function withContent(string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * Getter for the content
+     */
+    public function content(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * This method converts the message to an array
+     *
+     * @return array The array representation of the response
+     */
+    public function toArray(): array
+    {
+        return [
+            'role'    => $this->role,
+            'content' => $this->content,
+        ];
+    }
+}
---
+++ src/Responses/TextResponse.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Illegal\LaravelAI\Responses;
+
+use Illegal\LaravelAI\Contracts\HasNew;
+
+class TextResponse
+{
+    use HasNew;
+
+    /**
+     * @var string $externalId - The external id of the text
+     */
+    private string $externalId;
+
+    /**
+     * @var MessageResponse $message - The message, in the MessageResponse format
+     */
+    private MessageResponse $message;
+
+    /**
+     * Setter for the external id
+     */
+    public function withExternalId(string $externalId): self
+    {
+        $this->externalId = $externalId;
+        return $this;
+    }
+
+    /**
+     * Getter for the external id
+     */
+    public function externalId(): string
+    {
+        return $this->externalId;
+    }
+
+    /**
+     * Setter for the message
+     */
+    public function withMessage(MessageResponse $message): self
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * Getter for the message
+     */
+    public function message(): MessageResponse
+    {
+        return $this->message;
+    }
+}
