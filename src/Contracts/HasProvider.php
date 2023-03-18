45d2ffd48d9d3fb96587d7c14ffba2d6d8798b97
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-18 14:14:33 +0100

    Adds docblocks for HasProvider trait

--- src/Contracts/HasProvider.php
+++ src/Contracts/HasProvider.php
@@ -6,11 +6,25 @@ use Illegal\LaravelAI\Enums\Provider;
 
 trait HasProvider
 {
-    public Provider $provider;
+    /**
+     * @var Provider $provider - The provider
+     */
+    private Provider $provider;
 
+    /**
+     * Setter for the provider
+     */
     public function withProvider(Provider $provider): self
     {
         $this->provider = $provider;
         return $this;
+    }
+
+    /**
+     * Getter for the provider
+     */
+    public function provider(): Provider
+    {
+        return $this->provider;
     }
 }
