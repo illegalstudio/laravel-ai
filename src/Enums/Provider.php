b1ca0f3cc10f3883c307f53d2179284341fac4b8
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-18 14:13:50 +0100

    Adds docblocks to Provider enum

--- src/Enums/Provider.php
+++ src/Enums/Provider.php
@@ -5,10 +5,21 @@ namespace Illegal\LaravelAI\Enums;
 use Illegal\LaravelAI\Contracts\Connector;
 use Illegal\LaravelAI\Connectors\OpenAIConnector;
 
+/**
+ * This is an enumeration of all possible providers
+ */
 enum Provider: string
 {
+    /**
+     * The OpenAI provider. This is the only one supported at the moment.
+     */
     case OpenAI = OpenAIConnector::NAME;
 
+    /**
+     * This method returns the connector for the provider
+     *
+     * @return Connector The connector for the provider
+     */
     public function getConnector(): Connector
     {
         return match ($this) {
