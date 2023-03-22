cb4970fa9ed7ab8feacf10bc0ea03fb4375f1f9b
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-22 14:05:00 +0100

    Added BelongsToModel usage on Completion and Chat

--- src/Models/Completion.php
+++ src/Models/Completion.php
@@ -2,10 +2,14 @@
 
 namespace Illegal\LaravelAI\Models;
 
+use Illegal\LaravelAI\Contracts\BelongsToModel;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Completion extends EloquentModel
 {
+    use BelongsToModel;
+
     protected $fillable = [
         'model_id',
         'external_id',
