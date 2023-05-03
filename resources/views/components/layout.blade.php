9cd56dfb691c7ce14f556c6e4df356362fe1e23d
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-05-03 15:34:48 +0200

    Layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,14 +2,14 @@
 <head>
     <title>Laravel AI</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     {{ Vite::useHotFile(base_path() . '/vendor/illegal/laravel-ai/public/hot')->useBuildDirectory('vendor/laravel-ai/linky')->withEntryPoints([ 'resources/css/app.scss' ]) }}
     @livewireStyles
 </head>
-<body class="">
+<body>
 
 {{ $slot }}
-
 
 {{ Vite::useHotFile(base_path() . '/vendor/illegal/laravel-ai/public/hot')->useBuildDirectory('vendor/illegal/laravel-ai')->withEntryPoints([ 'resources/js/app.js' ]) }}
 @livewireScripts
